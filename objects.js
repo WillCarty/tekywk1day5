d78a7baa047ad633392c75959479e75e666ac3f5
William Carty <will.carty@interapthq.com>
2016-09-16 12:01:58 -0400

    ojects

---
+++ objects.js
@@ -0,0 +1,54 @@
+
+/*
+var will = {
+    firstName: "William",
+    lastName: "Carty",
+    email: "will.carty@interapthq.com",
+    phone: "606.496.7132",
+    birthDay: "11.26.1984",
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+function printDetails(p) {
+    console.log(p.firstName);
+    console.log(p.lastName);
+    console.log(p.email);
+    console.log(p.phone);
+    console.log(p.birthDay);
+    console.log(p.fullName());
+}
+
+printDetails(will)
+*/
+/*
+var will = {
+    firstName: "William",
+    lastName: "Carty",
+
+    phone: "606.496.7132",
+    
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+function printDetails(p) {
+    if (typeof p.email != "undefined") {
+        
+        console.log(p.email)
+        }
+    console.log(p.firstName);
+    console.log(p.lastName);
+    console.log(p.email);
+    console.log(p.phone);
+    console.log(p.birthDay);
+    console.log(p.fullName());
+}
+
+printDetails(will)
+*/
+
+var number=Math.floor(500*3);
+console.log(number);
